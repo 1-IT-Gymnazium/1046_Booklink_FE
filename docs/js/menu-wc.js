84a299dc5eca296f7ed346439fbcfa4a66156db1
1046 <1046@student.itgymnazium.cz>
2025-04-01 23:53:30 +0200

    Generate and publish Compodoc documentation

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -45,6 +45,158 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                     </ul>
                 </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-bs-toggle="collapse" ${ isNormalMode ?
+                                'data-bs-target="#modules-links"' : 'data-bs-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/AppRoutingModule.html" data-type="entity-link" >AppRoutingModule</a>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ButtonComponent.html" data-type="entity-link" >ButtonComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardComponent.html" data-type="entity-link" >DashboardComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DatabaseComponent.html" data-type="entity-link" >DatabaseComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ForgotPasswordComponent.html" data-type="entity-link" >ForgotPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeaderComponent.html" data-type="entity-link" >HeaderComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginComponent.html" data-type="entity-link" >LoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NavBarComponent.html" data-type="entity-link" >NavBarComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PageComponent.html" data-type="entity-link" >PageComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RegisterComponent.html" data-type="entity-link" >RegisterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ResetPasswordComponent.html" data-type="entity-link" >ResetPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/UserProfileComponent.html" data-type="entity-link" >UserProfileComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ValidateTokenComponent.html" data-type="entity-link" >ValidateTokenComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/DatabaseService.html" data-type="entity-link" >DatabaseService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#guards-links"' :
+                            'data-bs-target="#xs-guards-links"' }>
+                            <span class="icon ion-ios-lock"></span>
+                            <span>Guards</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="guards-links"' : 'id="xs-guards-links"' }>
+                            <li class="link">
+                                <a href="guards/AuthGuard.html" data-type="entity-link" >AuthGuard</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/Book.html" data-type="entity-link" >Book</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Bookshelf.html" data-type="entity-link" >Bookshelf</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ForgotPasswordRequest.html" data-type="entity-link" >ForgotPasswordRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Household.html" data-type="entity-link" >Household</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ResetPasswordRequest.html" data-type="entity-link" >ResetPasswordRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Room.html" data-type="entity-link" >Room</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UpdateBookRequest.html" data-type="entity-link" >UpdateBookRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UpdateBookshelfRequest.html" data-type="entity-link" >UpdateBookshelfRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UpdateEntityRequest.html" data-type="entity-link" >UpdateEntityRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/User.html" data-type="entity-link" >User</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
                     <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
